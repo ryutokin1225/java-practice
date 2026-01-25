04418e72d7182bb316e46132638350655a65b412
LIU DONGQIN <ryutokin1225@gmail.com>
2026-01-25 19:36:22 +0900

    update

--- FirstJava.java
+++ FirstJava.java
@@ -1,5 +1,16 @@
+package liu.dong.qin;
+
+class SecondJava{
+    public static void main(String[] args) {
+    int age = 18;
+    System.out.println("私の年齢は" + age);
+    age = 29;
+    System.out.println("…いや、本当の年齢は" + age);
+    }
+}
+
 public class FirstJava {
     public static void main(String[] args) {
         System.out.println("リュウトウキン");
-    }
+        } 
 }
